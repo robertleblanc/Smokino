4ca7185d821a5fa007ca06af50c41fa62eb8732d
Robert LeBlanc <robert.leblanc365@gmail.com>
2015-08-01 20:53:57 -0600

    Added an activity in the manifest along with launcher and main. Added Smokino activity that is blank.

---
+++ app/src/main/java/rleblanc/ca/smokino/SmokinoGUI.java
@@ -0,0 +1,10 @@
+package rleblanc.ca.smokino;
+
+import android.app.Activity;
+
+/**
+ * Created by Robert LeBlanc - Aug 1, 2015
+ */
+public class SmokinoGUI extends Activity {
+    private final String TAG = "SmokinoGUI: ";
+}
