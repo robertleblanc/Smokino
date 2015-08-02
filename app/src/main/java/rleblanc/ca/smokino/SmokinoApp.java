cc52388de558039aa8cbdb64640e43537ab9ba8b
Robert LeBlanc <robert.leblanc365@gmail.com>
2015-08-02 13:04:11 -0600

    StartUpActivity now prompts to enable bluetooth and then launches the SmokinoGUI activity

--- app/src/main/java/rleblanc/ca/smokino/SmokinoApp.java
+++ app/src/main/java/rleblanc/ca/smokino/SmokinoApp.java
@@ -1,12 +1,13 @@
 package rleblanc.ca.smokino;
 
 import android.app.Application;
+import android.bluetooth.BluetoothAdapter;
 
 /**
  * Created by Robert LeBlanc - Aug 1, 2015
  */
 public class SmokinoApp extends Application {
-    BTDevice btDevice;
+    public BluetoothAdapter mBluetoothAdapter;
 
     static SmokinoApp smokinoApp;
 
@@ -17,6 +18,13 @@ public class SmokinoApp extends Application {
     @Override
     public void onCreate() {
         smokinoApp = this;
+
+        //Check to see that the device running this app has a bluetooth adapter
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            System.exit(0); // Device does not support Bluetooth
+        }
+
         super.onCreate();
     }
 }
---
+++ app/src/main/java/rleblanc/ca/smokino/StartUpActivity.java
@@ -0,0 +1,48 @@
+package rleblanc.ca.smokino;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+/**
+ * Created by Robert LeBlanc - Aug 1, 2015
+ */
+public class StartUpActivity extends Activity {
+
+    /* Constants */
+    private final int REQUEST_ENABLE_BT = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+
+        //Check to see if bluetooth is enabled
+        if (!BluetoothAdapter.getDefaultAdapter().isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request we're responding to
+        if (requestCode == REQUEST_ENABLE_BT) {
+            // Make sure the request was successful
+            if (resultCode == RESULT_OK) {
+                startActivity(new Intent(this, SmokinoGUI.class));
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "You need to enable bluetooth to use this app - Do it now or else...", Toast.LENGTH_SHORT).show();
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            } else {
+                Toast.makeText(this, "An error likely occured while trying to enable bluetooth -- Try again", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+}
